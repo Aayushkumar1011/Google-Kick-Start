8e3b560c23ce2321eca582e548a76c285a9e61e9
Ayush kumar bharti <aayushkumar1011@gmail.com>
2022-07-10 18:35:17 +0530

    Update and rename InputFormat.java to MaximumGain.java

--- Java/MaximumGain.java
+++ Java/MaximumGain.java
@@ -16,7 +16,7 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Main
+class MaximumGain
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
