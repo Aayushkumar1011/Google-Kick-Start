df2f757edce933e688439d2dd504540e094ecffc
Ayush kumar bharti <aayushkumar1011@gmail.com>
2022-07-10 19:14:03 +0530

    Update MaximumGain.java

--- Java/MaximumGain.java
+++ Java/MaximumGain.java
@@ -16,8 +16,9 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class MaximumGain
+class Main
 {
+    public static int ans = 0;
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
@@ -29,6 +30,7 @@ class MaximumGain
             // Read an int value
             int n = sc.nextInt();
             int[] arr1 = new int[n];
+            // int ind = 0;
             for(int i = 0; i < n; i++){
                 int ele = sc.nextInt();
                 arr1[i] = ele;
@@ -50,27 +52,35 @@ class MaximumGain
             }
 
 
-            System.out.println("Case #" + ": " + t + "- A: " + Arrays.toString(arr1));
-            System.out.println("Case #" + ": " + t + "- B: " + Arrays.toString(arr2));
-            // int res = getMax(arr1, arr2, n, m, k, dp);
-            // System.out.println("Case #" + t + ": " + res);
+            // System.out.println("Case #" + ": " + t + "- A: " + Arrays.toString(arr1));
+            // System.out.println("Case #" + ": " + t + "- B: " + Arrays.toString(arr2));
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+            int res = getMax(arr1, arr2, n, m, k, dp);
+            System.out.println("Case #" + t + ": " + res);
         }
         
         // System.out.println("Arr: " + Arrays.toString(arr));
 	}
-	
-	public static int getMax(int[] arr1, int arr2, int n, int m, int k, int[][] dp){
-        int res = 0;
-        // 
-        int i = 1, ii = 1;
-        while(i < n && )
-                for(int j = 1; j <= k; j++){
-                    if(arr1[i] >= arr2[ii]){
 
-                    }
-                }
-            }
-        }
-	    return res;
-	}
+    public static int getMax(int[] arr1, int[] arr2, int n, int m, int k, int[][] dp){
+        if(n < 0 && m < 0)
+            return 0;
+        if(k <= 0)
+            return 0;
+        if(n < 0 && m >= 0)
+            Math.max(ans,
+                ans + arr2[m-1] + getMax(arr1, arr2, n, m-1, k-1, dp));
+        if(m < 0 && n >= 0)
+            return Math.max(ans,
+                ans + arr1[n-1] + getMax(arr1, arr2, n-1, m, k-1, dp));
+        
+        if(arr1[n-1] > arr2[m-1])
+            return Math.max(ans,
+                ans + arr1[n-1] + getMax(arr1, arr2, n-1, m, k-1, dp));
+        else 
+            return Math.max(ans,
+                ans + arr2[m-1] + getMax(arr1, arr2, n, m-1, k-1, dp));
+
+    }
 }
