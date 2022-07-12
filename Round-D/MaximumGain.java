1c0b23572a2634fb785c96f81909482943c3a3a2
Ayush kumar bharti <aayushkumar1011@gmail.com>
2022-07-12 21:03:09 +0530

    Update MaximumGain.java

--- Round-D/MaximumGain.java
+++ Round-D/MaximumGain.java
@@ -15,52 +15,48 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class MaximumGain
-{
+class MaximumGain {
 
-	
-    public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-		Scanner sc = new Scanner(System.in);
-		int test = sc.nextInt();
-		
-		for(int t = 1; t <= test; t++){
-            // Read an int value
-            int n = sc.nextInt();
-            int[] arr1 = new int[n];
-            for(int i = 0; i < n; i++){
-                arr1[i] = sc.nextInt();;
-            }
+  public static void main(String[] args) throws java.lang.Exception {
+    // your code goes here
+    Scanner sc = new Scanner(System.in);
+    int test = sc.nextInt();
 
-            int m = sc.nextInt();
-            int[] arr2 = new int[m];
-            for(int i = 0; i < m; i++){
-                arr2[i] = sc.nextInt();;
-            }
-            int k = sc.nextInt();
-            int res = getMax(arr1, arr2, 0, n, 0, m, k);
-            System.out.println("Case #" + t + ": " + res);
-        }
-	}
+    for (int t = 1; t <= test; t++) {
+      // Read an int value
+      int n = sc.nextInt();
+      int[] arr1 = new int[n];
+      for (int i = 0; i < n; i++) {
+        arr1[i] = sc.nextInt();;
+      }
 
-    public static int getMax(int[] arr1, int[] arr2, int s1, int n, int s2, int m, int k){
-        if(k <= 0)
-            return 0;
-        if(s1 > n-1 && s2 > m-1)
-            return 0;
-				int ans1 = 0;
-				int ans2 = 0;
-				if(s1 < n){
-							ans1 = Math.max(arr1[s1] + getMax(arr1, arr2, s1+1, n, s2, m, k-1),
-											arr1[n-1] + getMax(arr1, arr2, s1, n-1, s2, m, k-1)); 
-				}
+      int m = sc.nextInt();
+      int[] arr2 = new int[m];
+      for (int i = 0; i < m; i++) {
+        arr2[i] = sc.nextInt();;
+      }
+      int k = sc.nextInt();
+      int res = getMax(arr1, arr2, 0, n, 0, m, k);
+      System.out.println("Case #" + t + ": " + res);
+    }
+  }
 
-				if(s2 < m){
-					ans2 = Math.max(arr2[s2] + getMax(arr1, arr2, s1, n, s2+1, m, k-1),
-								arr2[m-1] + getMax(arr1, arr2, s1, n, s2, m-1, k-1)); 
-				}
-				return Math.max(ans1, ans2);
-				}
+  public static int getMax(int[] arr1, int[] arr2, int s1, int n, int s2, int m, int k) {
+    if (k <= 0)
+      return 0;
+    if (s1 > n - 1 && s2 > m - 1)
+      return 0;
+    int ans1 = 0;
+    int ans2 = 0;
+    if (s1 < n) {
+      ans1 = Math.max(arr1[s1] + getMax(arr1, arr2, s1 + 1, n, s2, m, k - 1),
+        arr1[n - 1] + getMax(arr1, arr2, s1, n - 1, s2, m, k - 1));
+    }
+
+    if (s2 < m) {
+      ans2 = Math.max(arr2[s2] + getMax(arr1, arr2, s1, n, s2 + 1, m, k - 1),
+        arr2[m - 1] + getMax(arr1, arr2, s1, n, s2, m - 1, k - 1));
+    }
+    return Math.max(ans1, ans2);
+  }
 }
-
