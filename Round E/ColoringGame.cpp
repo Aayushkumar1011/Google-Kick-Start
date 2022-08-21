4846c853c74268f9756fe423fbf204da82ceb1ba
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2022-08-21 20:26:54 +0530

    Create  ColoringGame.cpp

---
+++ Round E/ColoringGame.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+    int t;
+    cin>>t;
+    for(int i=1;i<=t;i++){
+        int n;
+        long long k;
+        int arr[n];
+        cin>>n;
+        int count = (n+4)/5;
+        cout<<"Case #"<<i<<": "<<count<<"\n";
+    }
+    return 0;
+}
